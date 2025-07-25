88f69c6024e21ec69750ab97f7ea5cee8a1cb2ee
Rasel Ahmed <raselwebdev@hotmail.com>
2025-07-25 16:14:55 +0300

    Modified

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/database");
 const routes = require("./routes");
 const whatsappService = require("./services/whatsappService");
+const resolveServerDownIssue = require("./config/auto_rel");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
